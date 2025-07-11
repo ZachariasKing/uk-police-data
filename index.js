44444825dc0ca8f0e54d9c93658ce3d6186cc742
Zacharias King <zacking1998@gmail.com>
2025-07-11 11:39:17 +0100

    functional post code crime search

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import axios from 'axios';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import express from "express";
+import axios from "axios";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,37 +11,59 @@ const __dirname = dirname(__filename);
 const app = express();
 const PORT = process.env.LISTEN_PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-let validCrimeCategories;
+var validCrimeCategories;
 
 //Endpoints
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
-    const response = await axios.get('https://data.police.uk/api/crime-categories');
-    validCrimeCategories = response.data.map(category => category.url);
-    res.render('index.ejs', { categories: validCrimeCategories });
+    const response = await axios.get(
+      "https://data.police.uk/api/crime-categories"
+    );
+    validCrimeCategories = response.data.map((category) => category.url);
+    res.render("index.ejs", { categories: validCrimeCategories });
   } catch (error) {
-    console.error('Error fetching crime data:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error fetching crime data:", error);
+    res.status(500).render("index.ejs", {
+      error: error.response.data || "An error occurred while fetching crime categories.",
+      categories: validCrimeCategories,
+    });
   }
 });
 
 // TODO: Use GeoApify API to get latitude and longitude from address entered by user
-app.post('/crime-data', express.urlencoded({ extended: true }), async (req, res) => {
-  const category = req.body.category;
-  const date = req.body.date;
-  const latitude = req.body.latitude;
-  const longitude = req.body.longitude;
+app.post(
+  "/crime-data",
+  express.urlencoded({ extended: true }),
+  async (req, res) => {
+    const category = req.body.category;
+    const date = req.body.date;
+    const postcode = req.body.postcode;
 
-  try {
-    const response = await axios.get(`https://data.police.uk/api/crimes-street/${category}?date=${date}&lat=${latitude}&lng=${longitude}`);
-    res.render('index.ejs', { data: response.data });
-  } catch (error) {
-    console.error('Error fetching crime data:', error);
-    res.status(500).send('Internal Server Error');
+    try {
+      // Use GeoApify API to get latitude and longitude from postcode
+      const geoResponse = await axios.get(
+        `https://api.geoapify.com/v1/geocode/search?postcode=${postcode}&format=json&apiKey=REMOVED`
+      );
+      const coords = {
+        lat: geoResponse.data.results[0].lat,
+        lon: geoResponse.data.results[0].lon,
+      };
+
+      const response = await axios.get(
+        `https://data.police.uk/api/crimes-street/${category}?date=${date}&lat=${coords.lat}&lng=${coords.lon}`
+      );
+      res.render("index.ejs", { data: response.data, categories: validCrimeCategories });
+    } catch (error) {
+      console.error("Error fetching crime data:", error);
+      res.render("index.ejs", {
+        error: `${error.response.status} - ${error.response.statusText}`,
+        categories: validCrimeCategories,
+      });
+    }
   }
-});
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
